272dd5eef1853f4b02383b8c0117de9d13f487f2
olive <olive@DESKTOP-27RE9Q5>
2024-07-10 02:41:49 -0400

    Agregando la clase Computadora

---
+++ proyectoFinal/src/logico/Computadora.java
@@ -0,0 +1,63 @@
+package logico;
+
+import java.util.ArrayList;
+
+public class Computadora {
+	
+	private String id;
+	private ArrayList<DiscoDuro> misDiscoDuros;
+	private ArrayList<MemoriaRAM> misMemoriaRAMs;
+	private TarjetaMadre miTarjetaMadre;
+	private Microprocesador microprocesador;
+	private ArrayList<String> adicionales; //esto es una idea mientras tanto, aqui estaran los monitores, teclado, raton, etc...
+	
+	
+	public Computadora(String id, ArrayList<DiscoDuro> misDiscoDuros, ArrayList<MemoriaRAM> misMemoriaRAMs,
+			TarjetaMadre miTarjetaMadre, Microprocesador microprocesador, ArrayList<String> adicionales) {
+		super();
+		this.id = id;
+		this.misDiscoDuros = misDiscoDuros;
+		this.misMemoriaRAMs = misMemoriaRAMs;
+		this.miTarjetaMadre = miTarjetaMadre;
+		this.microprocesador = microprocesador;
+		this.adicionales = adicionales;
+	}
+	
+	public ArrayList<DiscoDuro> getMisDiscoDuros() {
+		return misDiscoDuros;
+	}
+	public void setMisDiscoDuros(ArrayList<DiscoDuro> misDiscoDuros) {
+		this.misDiscoDuros = misDiscoDuros;
+	}
+	public ArrayList<MemoriaRAM> getMisMemoriaRAMs() {
+		return misMemoriaRAMs;
+	}
+	public void setMisMemoriaRAMs(ArrayList<MemoriaRAM> misMemoriaRAMs) {
+		this.misMemoriaRAMs = misMemoriaRAMs;
+	}
+	public TarjetaMadre getMiTarjetaMadre() {
+		return miTarjetaMadre;
+	}
+	public void setMiTarjetaMadre(TarjetaMadre miTarjetaMadre) {
+		this.miTarjetaMadre = miTarjetaMadre;
+	}
+	public Microprocesador getMicroprocesador() {
+		return microprocesador;
+	}
+	public void setMicroprocesador(Microprocesador microprocesador) {
+		this.microprocesador = microprocesador;
+	}
+	public ArrayList<String> getAdicionales() {
+		return adicionales;
+	}
+	public void setAdicionales(ArrayList<String> adicionales) {
+		this.adicionales = adicionales;
+	}
+	public String getId() {
+		return id;
+	}
+	
+	
+	
+	
+}
